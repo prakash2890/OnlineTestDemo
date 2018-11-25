0bf9429f5f9c7303809187a249ebc9d055d3d315
prakash2890 <prakashraj2111@gmail.com>
2018-11-25 16:04:01 +0530

    Final Commit

---
+++ OnlineTest/OnlineTest/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace OnlineTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DataTable dt = new DataTable();
+        string cn = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+        public void BindData()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("select * from tblQuestions", cn);
+            sda.Fill(dt);
+            grdquestions.DataSource = dt;
+            grdquestions.DataBind();
+        }
+
+        protected void grdquestions_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                RadioButtonList rdbOptions =(RadioButtonList) e.Row.FindControl("rdlstOptions");
+                HiddenField hdnQuestionID = (HiddenField)e.Row.FindControl("hdnQuestoinID");
+                if (rdbOptions != null && hdnQuestionID != null)
+                {
+                    DataRow[] result = dt.Select("questionID=" + Convert.ToInt32(hdnQuestionID.Value));
+                    DataView view = new DataView();
+                    view.Table = dt;
+                    view.RowFilter = "questionID=" + Convert.ToInt32(hdnQuestionID.Value);
+                    if (view.Table.Rows.Count > 0)
+                    {
+                        DataTable dt1 = new DataTable();
+                        dt1 = view.ToTable();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OnlineTest/OnlineTest/index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineTest
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["timeout"] != null)
+            {
+                lbstatus.Text = "Your Test time expired, please try again..!";
+            }
+        }
+    }
+}
